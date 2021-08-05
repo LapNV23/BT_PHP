7af16bf927a39fd9bbad09bb859b606194ce2fd9
LapNV23 <73330679+LapNV23@users.noreply.github.com>
2021-08-05 19:55:23 +0700

    Task3

---
+++ Task3/update.php
@@ -0,0 +1,141 @@
+<?php
+require_once 'config.php';
+$name = $address = $salary ="";
+$name_err = $address_err = $salary_err = "";
+if (isset($_POST["id"]) && !empty($_POST["id"])){
+    $id = $_POST["id"];
+
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)) {
+        $name_err = "Please enter a name.";
+    }
+    }elseif (!filter_var(trim($_POST["name"]), FILTER_VALIDATE_REGEXP,
+        array("options"=>array("regexp"=>"/^[a-zA-Z'-.\s]+$")))){
+        $name_err = 'Please enter a valid name.';
+    }else{
+        $name = $input_name;
+    }
+
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)) {
+        $address_err = "Please enter a address.";
+    }else{
+        $address = $input_address;
+    }
+
+    $input_salary = trim($_POST["salary"]);
+    if(empty($input_salary)) {
+        $salary_err = "Please enter a salary.";
+    }elseif (!ctype_digit($input_salary)) {
+        $salary_err = 'Please enter a positive integer value.';
+    }else{
+        $price = $input_salary;
+    }
+
+
+    if(empty($name_err)&&empty($address_err)&&empty($salary_err)){
+        $sql = "UPDATE employees SET name =?, address=?, salary=? WHERE id=?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt,"sssi",$param_name, $param_address, $param_salary, $param_id);
+
+            $param_name = $name;
+            $param_address = $address;
+            $param_salary = $salary;
+            $param_id = $id;
+
+            if (mysqli_stmt_execute($stmt)){
+                header("location: index.php");
+                exit();
+            }else{
+                echo "Something went wrong. please try again later";
+            }
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($link);
+
+ else{
+    if (isset($_POST["id"]) && !empty($_POST["id"])){
+        $id = $_POST["id"];
+
+        $sql = "SELECT * FROM employees WHERE id=?";
+        if ($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "i", $param_id);
+            $param_id = $id;
+
+            if (mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+                if (mysqli_num_rows($result)==1){
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+                    $id = $row["id"];
+                    $name = $row["name"];
+                    $address = $row["address"];
+                    $salary = $row["salary"];
+
+                }else{
+                    header("location: error.php");
+                    exit();
+                }
+            }else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        mysqli_stmt_close($stmt);
+        mysqli_close($link);
+    }else{
+        header("location: error.php");
+        exit();
+    }
+}
+?>
+<!DOCTYPe html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Record</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css ">
+        .wrappers{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+<div class="wrappers">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h1>Update Record</h1>
+                </div>
+                <p>Please edit the input values and submit to update the record.</p>
+                <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                    <div class="form-group <?php echo (!empty($id_err))? 'has-error':'';?>">
+                        <label>ID</label>
+                        <input type="text" name="id" class="form-control" value="<?php echo $id;?>">
+                        <span class="help-block"><?php echo $id_err; ?></span>
+                    </div>
+                    <div class="form-group <?php echo (!empty($title_err))? 'has-error':'';?>">
+                        <label>Address</label>
+                        <input type="text" name="title" class="form-control" value="<?php echo $address;?>">
+                        <span class="help-block"><?php echo $address_err; ?></span>
+                    </div>
+                    <div class="form-group <?php echo (!empty($author_err))? 'has-error':'';?>">
+                        <label>Salary</label>
+                        <input type="text" name="author" class="form-control" value="<?php echo $salary;?>">
+                        <span class="help-block"><?php echo $salary_err; ?></span>
+                    </div>
+
+
+                    <input type="hidden" name="id" value="<?php echo $id;?>"/>
+                    <input type="submit" class="btn btn-primary" value="submit">
+                    <a href="index.php" class="btn btn-defauft">Cancel</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
