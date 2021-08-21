43809a5bb7b9fc15f2ccacfb64e68090aba98756
LapNV23 <73330679+LapNV23@users.noreply.github.com>
2021-08-21 20:28:58 +0700

    Project_Laravel

---
+++ Project Laravel/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\Front\HomeController::class, 'index'] );
+
+Route::prefix('shop')->group(function () {
+    Route::get('/product/{id}', [\App\Http\Controllers\Front\ShopController::class, 'show']);
+    Route::post('/product/{id}', [\App\Http\Controllers\Front\ShopController::class, 'postComment']);
+
+    Route::get('/', [\App\Http\Controllers\Front\ShopController::class, 'index']);
+
+    Route::get('/{categoryName}', [App\Http\Controllers\Front\ShopController::class, 'category']);
+
+});
+
+Route::prefix('cart')->group(function (){
+    Route::get('add/{id}', [App\Http\Controllers\Front\CartController::class, 'add']);
+});
+
