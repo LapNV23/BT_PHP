379339046a47bc67ba2dedf960c0072865c22ef0
LapNV23 <73330679+LapNV23@users.noreply.github.com>
2021-09-08 16:59:39 +0700

    BaiThi

---
+++ BaiThi/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Route::get('/', function (){
+//    return \App\Models\Web::all();
+//});
+Route::get('/',[\App\Http\Controllers\Front\HomeController::class,'index']);
+Route::post('index',[\App\Http\Controllers\Front\HomeController::class,'store'])->name('store');
+Route::post('/',[\App\Http\Controllers\Front\HomeController::class,'index'])->name('index');
+Route::get('/search',[\App\Http\Controllers\Front\HomeController::class,'search']);
+Route::get('/',[\App\Http\Controllers\Front\HomeController::class,'getDashboard']);
